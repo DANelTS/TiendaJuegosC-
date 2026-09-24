d69e13652150e671a9b8b989bd1c24907a303539
DANelTS <damagetersecond@gmail.com>
2026-09-24 02:44:36 +0200

    Completada la clase Purchase, al menos la preliminar

--- TiendaJuegosC#/TiendaJuegosC#/Purchase.cs
+++ TiendaJuegosC#/TiendaJuegosC#/Purchase.cs
@@ -2,7 +2,21 @@
 
 public class Purchase
 {
-	public Purchase()
+	private Customer customer;
+	private Game game;
+	private int quantity;
+	private double totalPrice;
+
+	public Purchase(Customer customer, Game game, int quantity)
 	{
+		this.customer = customer;
+		this.game = game;
+		this.quantity = quantity;
+
+		totalPrice = game.Price * quantity;
 	}
+
+	public Customer Customer { get; }
+	public Game Game { get; }
+	public int Quantity { get; }
 }
